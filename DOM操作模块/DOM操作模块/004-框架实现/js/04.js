694f2aab774b998bef7108d10f533e067dcdc8a9
RookieDay <1109318841@qq.com>
2016-11-29 15:49:42 +0800

    update function
    update function

--- DOM操作模块/DOM操作模块/004-框架实现/js/04.js
+++ DOM操作模块/DOM操作模块/004-框架实现/js/04.js
@@ -1,10 +1,10 @@
 // // 构造函数
-// var itcast = function(selector) {
-//     return new itcast.fn.init(selector);
+// var ana = function(selector) {
+//     return new ana.fn.init(selector);
 // };
 // // 核心原型
-// itcast.fn = itcast.prototype = {
-//     constructor: itcast,
+// ana.fn = ana.prototype = {
+//     constructor: ana,
 //     selector: null,
 //     init: function(selector) {
 //         // 字符串: 选择器, html
@@ -18,10 +18,10 @@
 //         this.selector = selector;
 //     }
 // };
-// itcast.fn.init.prototype = itcast.prototype;
+// ana.fn.init.prototype = ana.prototype;
 
 // // 可扩展
-// itcast.extend = itcast.fn.extend = function(obj) {
+// ana.extend = ana.fn.extend = function(obj) {
 //     // 将 obj 的成员加到 this 上
 //     var k;
 //     for (k in obj) {
@@ -54,10 +54,10 @@
 // };
 
 // // 基本的工具方法
-// itcast.extend({
+// ana.extend({
 //     each: function(arr, fn) {
 //         var i, l = arr.length,
-//             isArray = itcast.isLikeArray(arr);
+//             isArray = ana.isLikeArray(arr);
 //         if (isArray) {
 //             // 数组
 //             for (i = 0; i < l; i++) {
@@ -78,7 +78,7 @@
 // });
 
 // // 判断类型的方法
-// itcast.extend({
+// ana.extend({
 //     isFunction: function(obj) {
 //         return typeof obj === 'function';
 //     },
@@ -88,7 +88,7 @@
 //     isLikeArray: function(obj) {
 //         return obj && obj.length && obj.length >= 0;
 //     },
-//     isItcast: function(obj) {
+//     isana: function(obj) {
 //         return !!obj.selector;
 //     },
 //     isDOM: function(obj) {
